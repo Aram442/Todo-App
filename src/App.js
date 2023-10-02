9fe27b34420ecb6995200cbffd2386609a349f91
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-02 16:59:32 +0300

    Install React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
-  return <div className="App">App work perfectly</div>;
+  return (
+    <div className="App">
+      <Button variant="contained">Contained</Button>
+    </div>
+  );
 }
 
 export default App;
