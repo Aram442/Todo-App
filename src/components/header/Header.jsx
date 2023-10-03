55696b87ea46178a54a83ada0956c3f7d568b93c
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-03 20:58:30 +0300

    Adding Icon Folder

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import IconButton from '@mui/material/IconButton';
+import { MoonIcon, SunIcon } from '../icons';
+import { useAuth } from "../../AuthContext";
 
 function Header() {
+  const { user, email, password, setUser, setEmail, setPassword } = useAuth();
+
   return (
     <div>
       <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-          </ul>
-        </nav>
+        <Grid container justifyContent="right">
+          {user ? (
+            <Button onClick={logout}>Logout</Button>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </Grid>
+        <Grid container justifyContent={"space-between"} mt={5} mb={3}>
+          <IconButton
+            color="primary"
+            aria-label="theme switcher"
+            onClick={setCurrentTheme}
+          >
+            {currentTheme === Theme.DARK ? <SunIcon /> : <MoonIcon />}
+          </IconButton>
+        </Grid>
       </header>
     </div>
   );
